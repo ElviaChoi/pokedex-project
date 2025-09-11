9e83e78a3d762d8c0193aa83e51a53d7a3f05b7a
Elvia Choi <elviachoi924@gmail.com>
2025-09-11 21:32:38 +0900

    Chore: Tailwindcss v4 마이그레이션
    - 'npx @tailwindcss/upgrade'를 사용하여 v4로 마이그레이션
    
    - 설정 파일 및 클래스 이름 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <>
-      <h1 className="border-t-[50px] border-t-[red] bg-black text-white text-[40px] text-center">
+      <h1 className="border-t-50 border-t-[red] bg-black text-white text-[40px] text-center">
         포켓몬 도감
       </h1>
       <nav className="py-[10px] border-b-[3px] border-b-black flex gap-[20px] justify-center">
