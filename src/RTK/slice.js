35084772e62fd95641026ceaeccdffb9948eae32
Elvia Choi <elviachoi924@gmail.com>
2025-09-11 21:39:45 +0900

    Feature: 초기 찜 목록 데이터 수정
    - 초기 찜 목록에 3, 5번 포켓몬 추가

--- src/RTK/slice.js
+++ src/RTK/slice.js
@@ -25,7 +25,7 @@ export const pokemonSlice = createSlice({
 
 export const favoriteSlice = createSlice({
   name: "favorite",
-  initialState: [1, 2, 4],
+  initialState: [1, 2, 3, 4, 5],
   reducers: {
     addToFavorite(state, action) {
       state.push(action.payload);
