28cf2eeb4089e575bd2b6b9674ceb3a5f8cbccdb
Elvia Choi <elviachoi924@gmail.com>
2025-09-12 04:37:32 +0900

    Style: 디테일 페이지 상하 여백 수정
    - 디테일 페이지의 카드 바깥쪽 회색 배경 상하 여백을 줄입니다.

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -9,7 +9,7 @@ export default function Detail() {
   const pokemon = useSelector(selectPokemonById(Number(pokemonId)));
 
   return (
-    <div className='min-h-screen flex justify-center items-center px-4 py-8'>
+    <div className='min-h-screen flex justify-center items-center px-4 py-4'>
       <div className='bg-white drop-shadow-[0_18px_30px_rgba(0,0,0,0.5)] transition-all duration-300 rounded-xl px-10 py-8 max-w-md w-full text-center hover:translate-y-[-4px] hover:shadow-[0_0_20px_5px_rgba(192,192,192,0.5),_0_0_35px_12px_rgba(255,255,255,0.7)]'>
         <div className='text-3xl font-bold mb-7'>
           {pokemon.name}
